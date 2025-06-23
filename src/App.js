5d1d0ff83c3b42678e709d3eb187516bc0ab9518
Akash Panchal <pa908114@gmail.com>
2025-06-24 00:09:04 +0530

    changes app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Form from "./assets/components/MobileUI/ServiceForm/Form";
 import SwiggyAppPage from "./assets/components/SwiggyClone/SwiggyAppPage";
 import ServiceTable from "./assets/components/ServiceTable/ServiceTable";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
   return (
     <>
+    <BrowserRouter basename="/trakky-react-tasks">
      <SwiggyAppPage />
       <Home />
       <Form />
      
       {/* <ServiceForm /> */}
       <ServiceTable />
+      </BrowserRouter>
     </>
   );
 }
